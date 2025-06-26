23ac0e9bf64d5847ac07c1ac847db98c3360ba99
JMKim <tdcian71@gmail.com>
2025-06-26 23:39:43 +0900

    made product sectionview, product row view

--- SwiftUIAutoScrollingTabs/View/Home.swift
+++ SwiftUIAutoScrollingTabs/View/Home.swift
@@ -12,6 +12,7 @@ struct Home: View {
     /// View Properties
     @State private var activeTab: ProductType = .iphone
     @Namespace private var animation
+    @State private var productsBasedOnType: [[Product]] = []
     
     var body: some View {
         /// For Auto Scrolling Content's
@@ -20,7 +21,9 @@ struct Home: View {
                 /// Lazy Stack For Pinning View at Top While Scrolling
                 LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders]) {
                     Section {
-                        
+                        ForEach(productsBasedOnType, id: \.self) { products in
+                            ProductSectionView(products)
+                        }
                     } header: {
                         ScrollableTabs()
                     }
@@ -31,6 +34,55 @@ struct Home: View {
         .toolbarBackground(.visible, for: .navigationBar)
         .toolbarBackground(Color(.purple), for: .navigationBar)
         .toolbarColorScheme(.dark, for: .navigationBar)
+        .background {
+            Rectangle()
+                .fill(Color(.systemGray6))
+                .ignoresSafeArea()
+        }
+        .onAppear {
+            /// Filtering Products Based on Product Type (Only Once)
+            guard productsBasedOnType.isEmpty else { return }
+            
+            for type in ProductType.allCases {
+                let products = products.filter { $0.type == type }
+                productsBasedOnType.append(products)
+            }
+        }
+    }
+    
+    /// Products Sectioned View
+    @ViewBuilder
+    func ProductSectionView(_ products: [Product]) -> some View {
+        VStack(alignment: .leading, spacing: 15) {
+            /// Safe Check
+            if let firstProduct = products.first {
+                Text(firstProduct.type.rawValue)
+                    .font(.title)
+                    .fontWeight(.semibold)
+            }
+            
+            ForEach(products) { product in
+                ProductRowView(product)
+            }
+        }
+        .padding(15)
+    }
+    
+    /// Product Row View
+    @ViewBuilder
+    func ProductRowView(_ product: Product) -> some View {
+        HStack(spacing: 15) {
+            Image(product.productImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .padding(10)
+                .background {
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        .fill(Color.white)
+                }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
     
     /// Scrollable Tabs
